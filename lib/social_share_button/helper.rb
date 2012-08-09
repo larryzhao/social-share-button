7345ff8b30a6fea4ea02953a8f1a82349355854d
lidashuang <ldshuang@gmail.com>
2012-08-09 13:58:30 +0800

    fix

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -4,6 +4,7 @@ module SocialShareButton
     def social_share_button_tag(title = "", opts = {})
       rel = opts[:rel]
       html = []
+      url = opts[:url]
       html << "<div class='social-share-button' data-title='#{title}' data-img='#{opts[:image]}'>"
       
       SocialShareButton.config.allow_sites.each do |name,user|
@@ -11,6 +12,7 @@ module SocialShareButton
         html << link_to("<i class=\"icon-#{name}\"></i>".html_safe, "#", :rel => "nofollow #{rel}", 
                         "data-site" => name, 
                         "data-user" => user,
+                        "data-url" => url;
                         :onclick => "return SocialShareButton.share(this);",
                         :title => h(link_title))
       end
